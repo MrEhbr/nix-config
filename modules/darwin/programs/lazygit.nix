a0a6c742d06c0b244745290ef0f62aa40f97f55a
Aleksei Burmistrov <mr.ehbr@gmail.com>
2026-01-23 23:20:53 +0200

    feat(lazygit): add keybindings and delta support

--- modules/darwin/programs/lazygit.nix
+++ modules/darwin/programs/lazygit.nix
@@ -5,15 +5,41 @@
       gui = {
         showIcons = true;
         nerdFontsVersion = "3";
+        sidePanelWidth = 0.2;
+        screenMode = "half";
+        enlargedSideViewLocation = "top";
+        showRandomTip = false;
+        showBottomLine = true;
+        showListFooter = true;
+        showFileTree = true;
+        showDivergenceFromBaseBranch = "arrowAndNumber";
+        experimentalShowBranchHeads = true;
+        filterMode = "fuzzy";
+        border = "rounded";
       };
-      customCommands = [ ];
+      keybinding = {
+        universal = {
+          scrollDownMain = "J";
+          scrollUpMain = "K";
+          scrollDownMain-alt1 = "<c-d>";
+          scrollUpMain-alt1 = "<c-u>";
+        };
+      };
+      promptToReturnFromSubprocess = false;
+      notARepository = "skip";
       git = {
+        autoFetch = true;
+        autoRefresh = true;
         commit = {
           signOff = true;
         };
         pagers = [
           {
-            externalDiffCommand = "difft --color=always";
+            externalDiffCommand = "difft --color=always --syntax-highlight on";
+          }
+          {
+            pager = "delta --paging=never";
+            colorArg = "always";
           }
         ];
         branchLogCmd = "git log --graph --color=always --abbrev-commit --decorate --date=relative --pretty=medium {{branchName}} --";
